d550c98934d6629d6c52eb0636b1d104eb3f910a
stingtam <stingtam@tom.com>
2024-08-19 10:12:52 +0800

    Add plugin Which-Key to utils.lua

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -1,79 +1,97 @@
 return {
-    {
-        "rhysd/accelerated-jk",
-        config = function()
-            vim.keymap.set("n", "j", "<Plug>(accelerated_jk_gj)")
-            vim.keymap.set("n", "k", "<Plug>(accelerated_jk_gk)")
+  {
+    "rhysd/accelerated-jk",
+    config = function()
+      vim.keymap.set("n", "j", "<Plug>(accelerated_jk_gj)")
+      vim.keymap.set("n", "k", "<Plug>(accelerated_jk_gk)")
+    end,
+  },
+  {
+    "folke/persistence.nvim",
+    config = function()
+      require("persistence").setup()
+      vim.keymap.set("n", "<leader>qs", [[<cmd>lua require("persistence").load()<cr>]], {})
+      vim.keymap.set("n", "<leader>ql", [[<cmd>lua require("persistence").load({ last = true })<cr>]], {})
+      vim.keymap.set("n", "<leader>qd", [[<cmd>lua require("persistence").stop()<cr>]], {})
+    end
+  },
+  {
+    "ethanholz/nvim-lastplace",
+    config = true,
+  },
+  {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    ---@type Flash.Config
+    opts = {},
+    -- stylua: ignore
+    keys = {
+      { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
+      { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
+      { "r", mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
+      { "R", mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+      -- { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+    },
+  },
+  -- {
+  --     "ellisonleao/glow.nvim",
+  --     config = true,
+  --     cmd = "Glow",
+  -- },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons",       -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    },
+    config = function()
+      require("neo-tree").setup({
+        window = {
+          position = "right",
+          width = 30,
+        },
+      })
+      vim.keymap.set({ "n", "v" }, "<leader>e", [[<cmd>Neotree toggle<CR>]])
+    end
+  },
+  {
+    "s1n7ax/nvim-window-picker",
+    config = function()
+      require("window-picker").setup({
+        filter_rules = {
+          include_current_win = true,
+          bo = {
+            filetype = { "fidget", "neo-tree" }
+          }
+        }
+      })
+      vim.keymap.set("n",
+        "<c-w>p",
+        function()
+          local window_number = require('window-picker').pick_window()
+          if window_number then vim.api.nvim_set_current_win(window_number) end
+        end
+      )
+    end
+  },
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+    keys = {
+      {
+        "<leader>?",
+        function()
+          require("which-key").show({ global = false })
         end,
+        desc = "Buffer Local Keymaps (which-key)",
+      },
     },
-    {
-        "folke/persistence.nvim",
-        config = function()
-            require("persistence").setup()
-            vim.keymap.set("n", "<leader>qs", [[<cmd>lua require("persistence").load()<cr>]], {})
-            vim.keymap.set("n", "<leader>ql", [[<cmd>lua require("persistence").load({ last = true })<cr>]], {})
-            vim.keymap.set("n", "<leader>qd", [[<cmd>lua require("persistence").stop()<cr>]], {})
-        end
-    },
-    {
-        "ethanholz/nvim-lastplace",
-        config = true,
-    },
-    {
-        "folke/flash.nvim",
-        event = "VeryLazy",
-        ---@type Flash.Config
-        opts = {},
-        -- stylua: ignore
-        keys = {
-            { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-            { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-            { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
-            { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-            -- { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
-        },
-    },
-    -- {
-    --     "ellisonleao/glow.nvim", 
-    --     config = true, 
-    --     cmd = "Glow",
-    -- },
-    {
-        "nvim-neo-tree/neo-tree.nvim",
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-            "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-            "MunifTanjim/nui.nvim",
-            -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-        },
-        config = function()
-            require("neo-tree").setup({
-                window = {
-                    position = "right",
-                    width = 30,
-                },
-            })
-            vim.keymap.set({"n", "v"}, "<leader>e", [[<cmd>Neotree toggle<CR>]])
-        end
-    },
-    {
-        "s1n7ax/nvim-window-picker",
-        config = function()
-            require("window-picker").setup({
-                filter_rules = {
-                    include_current_win = true,
-                    bo = {
-                        filetype = { "fidget", "neo-tree" }
-                    }
-                }
-            })
-            vim.keymap.set("n",
-                "<c-w>p",
-                function()
-                    local window_number = require('window-picker').pick_window()
-                    if window_number then vim.api.nvim_set_current_win(window_number) end
-                end
-            )
-        end
-    },
+  }
 }
